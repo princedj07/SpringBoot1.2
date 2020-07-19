8b859f8db98debeceea948ee9061e7410a2249c6
Prince <Prince@LAPTOP-KFIKPE1S>
2020-07-19 19:55:08 +0530

    First Commit

---
+++ 24.SpringBootwebsocket-callback/src/main/java/com/springboot/rest/domain/SMS.java
@@ -0,0 +1,27 @@
+package com.springboot.rest.domain;
+
+public class SMS {
+	private String to;
+	private String message;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "SMS{" + "to='" + to + '\'' + ", message='" + message + '\'' + '}';
+	}
+}
